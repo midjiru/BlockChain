9b68814cf69e97418d64d849911ac1e2078fcfad
midjiru <lovemr.jason@gmail.com>
2024-07-23 16:06:13 +0700

    new contract

---
+++ tips.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract tips {
+    address owner;
+    constructor(){
+        owner = msg.sender;
+    }
+    //1.put fund in smart contract
+
+    //2.view balance
+
+    //3.add user
+
+    //4.remove user
+
+    //5.view user
+
+    //6.distribute tips
+
+}
