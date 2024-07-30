37f1ef45886aa141d0e66859584385731da4722d
midjiru <lovemr.jason@gmail.com>
2024-07-30 15:25:09 +0700

    gumaivaiRa

--- tips.sol
+++ tips.sol
@@ -8,7 +8,15 @@ contract tips {
     }
     //1.put fund in smart contract
 
+    function addtips() payable public {
+
+    }
+
     //2.view balance
+
+    function viewtips() public view returns(uint){
+        return address(this).balance;
+    }
 
     //3.add user
 
