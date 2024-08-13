6d0ad8745cda04287f45c6fb97ac2fdca8da5b78
midjiru <lovemr.jason@gmail.com>
2024-08-13 15:25:07 +0700

    3-5

--- tips.sol
+++ tips.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.26;
 
 contract tips {
     address owner;
+    Waitress[]waitress;
+
     constructor(){
         owner = msg.sender;
     }
@@ -18,11 +20,33 @@ contract tips {
         return address(this).balance;
     }
 
-    //3.add user
+    //3.add waitress
+    struct Waitress{
+        address payable walletAddress;
+        string name;
+    }
+    //3.2 add waitress
+    function addWaitress(address payable walletAddress,string memory name)public {
+        bool waitressExist = false;
+
+        if (waitress.length >=1){
+            for(uint i=0; i<waitress.length; i++){
+                if (waitress[i].walletAddress == walletAddress){
+                    waitressExist = true;
+                }
+            }
+        }
+        if(waitressExist==false){
+            waitress.push(Waitress(walletAddress,name));
+        }
+    }
 
     //4.remove user
 
     //5.view user
+    function viewWaitress() public view returns(Waitress[] memory){
+        return waitress;
+    }
 
     //6.distribute tips
 
